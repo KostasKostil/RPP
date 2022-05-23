e6634539d0d2999b38236958777fa761ad6bf9f0
Костя Луценко <lutsenkos2016@gmail.com>
2022-05-23 11:22:22 +0300

    Initial

---
+++ MPI.cpp
@@ -0,0 +1,79 @@
+// MPI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <ctime>
+#include "mpi.h"
+
+using namespace std;
+
+typedef long long ll;
+
+const ll mod = 1'000'000'000'000'000'003;
+
+ll multiply(ll a, ll b)
+{
+    ll res = 0;
+    while (b > 0)
+    {
+        if (b & 1) res = (res + a) % mod;
+        a = (a + a) % mod;
+        b /= 2;
+    }
+    return res;
+}
+
+int main(int argc, char **argv)
+{
+    int ProcNum, ProcRank;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    ll n;
+    ll begin;
+    if (ProcRank == 0)
+    {
+        cout << "Input n: ";
+        cin >> n;
+        begin = clock();
+    }
+    MPI_Bcast(&n, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    
+    ll ans = 1;
+    for (ll i = ProcRank; i <= n; i += ProcNum)
+        if (i != 0)
+            ans = multiply(ans, i);
+    if (ProcRank != 0)
+        MPI_Send(&ans, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
+    else
+    {
+        ll nt; 
+        MPI_Status status;
+        for (int i = 1; i < ProcNum; i++)
+        {
+            MPI_Recv(&nt, 1, MPI_LONG_LONG, i, 0, MPI_COMM_WORLD, &status);
+            ans = multiply(ans, nt);
+        }
+
+        cout << "n! = " << ans << " mod 10^18+3\n";
+
+        auto end = clock();
+        cout << "Time: " << (end - begin) * 1.0 / CLOCKS_PER_SEC << "s\n";
+    }
+
+    MPI_Finalize();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
